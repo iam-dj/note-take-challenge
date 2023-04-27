21ea6bbd239bbc3e01cc81ce8b7900e63e280492
iam-dj <6887240+iam-dj@users.noreply.github.com>
2023-04-26 22:36:23 -0700

    added delete which was intense

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 const uuid = require("uuid");
+const randomNum = uuid.v4();
+
 
 function generateRandomNumber() {
-  const randomNum = Math.floor(Math.random() * 10000);
-  return randomNum;
+  const newNoteId = randomNum.slice(0,4);
+
+  return newNoteId;
 }
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -67,7 +71,7 @@ app.delete("/api/notes/:id", (req, res) => {
         const noteIndex = notes.findIndex((note) => note.id == noteID);
         if (noteIndex !== -1) {
           notes.splice(noteIndex, 1);
-          fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+          fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (err) => {
             if (err) {
               return res.status(500).json({ msg: "error writing db" });
             } else {
